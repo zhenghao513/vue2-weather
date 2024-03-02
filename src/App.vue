2d5056b42eef4ac9db5c4eb103bb7ae0e9b9b290
Zheng Hao <zhengh513@outlook.com>
2024-03-02 13:45:05 +0800

    feat: 新增实时天气组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="container">
     <h2>{{ locationName }}</h2>
+    <real-time
+      :temp="temp"
+      :text="text"
+      :tempMax="tempMax"
+      :tempMin="tempMin"
+    />
   </div>
 </template>
 
 <script>
+import RealTime from "./components/RealTime.vue";
 export default {
+  components: { RealTime },
   data() {
     return {
       locationName: "",
+      temp: 0,
+      text: "",
+      tempMax: 0,
+      tempMin: 0,
     };
   },
 };
---
+++ src/components/RealTime.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="real-time">
+    <div class="temp">
+      <span class="temp-number">{{ temp }}</span>
+      <span class="temp-symbol">℃</span>
+    </div>
+
+    <div class="weather">
+      <span class="weather-text">{{ text }}</span>
+      <span>&nbsp;</span>
+      <span class="weather-range">{{ tempMax }}°/{{ tempMin }}°</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    temp: {
+      type: Number,
+      default: 0,
+    },
+    text: {
+      type: String,
+      default: "多云",
+    },
+    tempMax: {
+      type: Number,
+      default: 0,
+    },
+    tempMin: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.temp {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 0.7;
+  transform: translateX(1rem);
+}
+
+.temp > span {
+  margin-top: 0;
+}
+
+.temp-number {
+  font-size: 7rem;
+}
+
+.temp-symbol {
+  align-self: flex-start;
+  font-size: 2.625rem;
+}
+
+.weather {
+  font-size: 1.2rem;
+}
+</style>
